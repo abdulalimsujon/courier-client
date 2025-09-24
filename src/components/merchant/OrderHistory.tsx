249b4e8ad2f7b017020f145228037e1169da6fa1
abdulalimsujon <alimsujon12@gmail.com>
2025-09-24 14:48:20 +0600

    update

--- src/components/merchant/OrderHistory.tsx
+++ src/components/merchant/OrderHistory.tsx
@@ -109,7 +109,7 @@ const OrderHistory = () => {
           textColor="#194185"
           description="Fill in the details below to start your delivery. We’ll take care of the rest."
         ></SectionHeading>
-        <div className="w-[385px] h-[56px] flex gap-[24px] opacity-100">
+        <div className="w-[385px] px-2 max-w-[385px] h-[56px] flex gap-[24px] opacity-100">
           <div className="bg-[#EF6820] w-[181px] h-[56px] flex items-center justify-center rounded-[8px] text-white font-medium">
             Regular
           </div>
@@ -120,25 +120,81 @@ const OrderHistory = () => {
         </div>
       </Container>
       <Container>
-        <div className="w-[792px] h-[296px]  rotate-0 opacity-100 flex flex-col  gap-[16px] ">
+        <div className="w-full max-w-[792px] text-[#717680] opacity-100 flex flex-col gap-[16px] mx-auto p-4">
           <input
-            className="bg-white w-full ss p-[16px] h-[56px] rounded-[8px] border border-[#B2DDFF] pt-[8px] pr-[24px] pb-[8px] pl-[24px] gap-[16px] rotate-0 opacity-100"
+            className="bg-white w-full h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]"
             placeholder="Parcel Name"
-          ></input>
+          />
+
           <input
-            className="bg-white w-full p-[16px] h-[56px] rounded-[8px] border border-[#B2DDFF] pt-[8px] pr-[24px] pb-[8px] pl-[24px] gap-[16px] rotate-0 opacity-100"
+            className="bg-white w-full h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]"
             placeholder="Parcel Weight"
-          ></input>
+          />
+
           <textarea
             rows={5}
-            cols={20}
-            className="bg-white  w-[792px] h-[113px] rounded-[8px]  pt-[8px] pr-[24px] pb-[8px] pl-[24px] gap-[16px] rotate-0 opacity-100 border border-[#B2DDFF] "
+            className="bg-white w-full h-[113px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]"
             placeholder="Description"
-          ></textarea>
+          />
+
           <input
-            className="bg-white w-full p-[16px] h-[56px] rounded-[8px] border border-[#B2DDFF] pt-[8px] pr-[24px] pb-[8px] pl-[24px] gap-[16px] rotate-0 opacity-100"
+            className="bg-white w-full h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]"
             placeholder="(Invoice If any)"
-          ></input>
+          />
+
+          <input
+            className="bg-white w-full h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]"
+            placeholder="Notes"
+          />
+
+          <select className="bg-white w-full h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px] text-sm sm:text-base">
+            <option className="text-sm sm:text-base" value="">
+              Select your Address
+            </option>
+            <option className="text-sm sm:text-base" value="invoice1">
+              Invoice 1
+            </option>
+            <option className="text-sm sm:text-base" value="invoice2">
+              Invoice 2
+            </option>
+          </select>
+
+          <select className="bg-white w-full h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]">
+            <option value="">Select your customer</option>
+            <option value="invoice1">Invoice 1</option>
+            <option value="invoice2">Invoice 2</option>
+          </select>
+
+          {/* Height */}
+          <div className="flex flex-col sm:flex-row items-center gap-2">
+            <label className="w-full sm:w-[120px] h-[56px] flex items-center">
+              Height (kg)
+            </label>
+            <input className="w-full bg-white h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]" />
+          </div>
+
+          {/* Weight */}
+          <div className="flex flex-col sm:flex-row items-center gap-2">
+            <label className="w-full sm:w-[120px] h-[56px] flex items-center">
+              Weight (kg)
+            </label>
+            <input className="w-full bg-white h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]" />
+          </div>
+
+          {/* Length */}
+          <div className="flex flex-col sm:flex-row items-center gap-2">
+            <label className="w-full sm:w-[120px] h-[56px] flex items-center">
+              Length (kg)
+            </label>
+            <input className="w-full bg-white h-[56px] rounded-[8px] border border-[#B2DDFF] px-[24px] py-[8px]" />
+          </div>
+
+          <button
+            className="bg-[#EF6820] text-white w-full h-[56px] rounded-[8px] px-[120px] py-[16px] 
+               opacity-100 transition hover:bg-[#d95b1c]"
+          >
+            Next
+          </button>
         </div>
       </Container>
     </div>
